e01052d98ed35458055f0263161602dbf0a4ea59
Connor Skorburg <connorskorburg@gmail.com>
2020-03-29 12:50:46 -0500

    removed ninja gold

--- ninja_gold/ninja_gold_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('process_money', views.process),
-    path('clear_session', views.clear_session)
-]
